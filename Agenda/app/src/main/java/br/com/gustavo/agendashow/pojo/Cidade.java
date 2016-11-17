c5f657c08dbf40941201115ac1692133f198d920
Gustavo Amorim <Gustavo Amorim>
2016-11-17 15:58:39 -0200

    Atualização4

--- Agenda/app/src/main/java/br/com/gustavo/agendashow/pojo/Cidade.java
+++ Agenda/app/src/main/java/br/com/gustavo/agendashow/pojo/Cidade.java
@@ -4,4 +4,33 @@ package br.com.gustavo.agendashow.pojo;
  * Created by Gustavo on 17/11/2016.
  */
 public class Cidade {
+        private long id_cidade;
+        private String nome;
+        private String bairro;
+
+        public Cidade(){}
+
+    public long getId_cidade() {
+        return id_cidade;
+    }
+
+    public void setId_cidade(long id_cidade) {
+        this.id_cidade = id_cidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
 }
--- Agenda/app/src/main/java/br/com/gustavo/agendashow/pojo/Musica.java
+++ Agenda/app/src/main/java/br/com/gustavo/agendashow/pojo/Musica.java
@@ -4,4 +4,34 @@ package br.com.gustavo.agendashow.pojo;
  * Created by Gustavo on 17/11/2016.
  */
 public class Musica {
+
+        private long id_musica;
+        private String nome;
+        private String comentario;
+
+        public Musica(){}
+
+    public long getId_musica() {
+        return id_musica;
+    }
+
+    public void setId_musica(long id_musica) {
+        this.id_musica = id_musica;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
 }
